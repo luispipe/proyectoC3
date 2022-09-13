8f96a5563990980686475b66d6c0203d6f7cd39f
luispipe <lflondonor@unal.edu.co>
2022-09-13 17:24:18 -0500

    cors configuration

--- authProject/settings.py
+++ authProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b)&!grzx_k283=0b!sw60og)0m5t)5pa^1w68cct67es-^^@+#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authApp',
+    'corsheaders',
 ]
 
 SIMPLE_JWT = {
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 REST_FRAMEWORK={
